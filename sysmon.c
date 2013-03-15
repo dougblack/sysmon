0d5c231147968024b2ddb429ce97b4a2bd538279
dougblack <dougblack@gatech.edu>
2013-03-15 10:48:29 -0400

    fixing memory bug and adding a cap on log size

--- sysmon.c
+++ sysmon.c
@@ -19,8 +19,8 @@ MODULE_LICENSE("GPL");
 #define log_name "sysmon_log"
 
 #define MAX_BUFFER_SIZE 1024
-#define MAX_LOG_LINES 90
 #define LOG_BLOCK_SIZE 30
+#define MAX_LOG_BLOCKS 100
 
 /* All of the syscalls we're interested in */
 static char *probe_names[] = {"sys_access", "sys_brk", "sys_chdir", "sys_chmod", "sys_clone", "sys_close", "sys_dup", "sys_dup2", "sys_execve", "sys_exit_group", "sys_fcntl", "sys_fork", "sys_getdents", "sys_getpid", "sys_gettid", "sys_ioctl", "sys_lseek", "sys_mkdir", "sys_mmap", "sys_munmap", "sys_open", "sys_pipe", "sys_read", "sys_rmdir", "sys_select", "sys_stat", "sys_fstat", "sys_lstat", "sys_wait4", "sys_write"};
@@ -38,16 +38,18 @@ static struct log_block {
 struct proc_dir_entry *uid_entry;
 struct proc_dir_entry *toggle_entry;
 struct proc_dir_entry *log_entry;
-static struct log_block head;
+static struct log_block *head;
 static struct log_block *current_block;
 
 static int toggle = 0;
 static int uid = -1;
+static int num_blocks;
 
 /* Called before each syscall */
 static int sysmon_intercept_before(struct kprobe *kp, struct pt_regs *regs)
 {
    int ret = 0;
+   struct log_block *temp;
    char *line = (char *) kmalloc(150 * sizeof(char), GFP_KERNEL);
 
    if (toggle == 0 || current->uid != uid)
@@ -64,6 +66,16 @@ static int sysmon_intercept_before(struct kprobe *kp, struct pt_regs *regs)
       /* We just filled the last line, build a new block */
       current_block->next = (struct log_block *) kmalloc(sizeof(struct log_block), GFP_KERNEL);
       current_block->next->id = current_block->id + 1;
+      num_blocks++;
+
+      /* Check if log is full. If so, drop first block. */
+      if (num_blocks > MAX_LOG_BLOCKS) {
+         temp = head;
+         printk(KERN_INFO MODULE_NAME "Dropping the block %d.\n", temp->id);
+         head = head->next;
+         kfree(temp->lines);
+         kfree(temp);
+      }
       current_block = current_block->next;
       current_block->line_count = 0;
       current_block->next = NULL;
@@ -92,7 +104,7 @@ int log_read(char *buffer, char **buffer_location, off_t offset, int buffer_leng
       ret_length = 0; 
    } else {
 
-      for (read_block = &head; read_block != NULL; lines_read++) {
+      for (read_block = head; read_block != NULL; lines_read++) {
 
          /* Print to buffer */
          printk("%s", read_block->lines[block_line]);
@@ -163,10 +175,11 @@ int init_module()
    }
 
    /* Set up the log linked list */
-   head.line_count = 0;
-   head.next = NULL;
-   head.id = 0;
-   current_block = &head;
+   head = (struct log_block *) kmalloc(sizeof(struct log_block), GFP_KERNEL);
+   head->line_count = 0;
+   head->next = NULL;
+   head->id = 0;
+   current_block = head;
 
    printk(KERN_INFO MODULE_NAME "added all the kprobes.\n");
 
@@ -208,8 +221,17 @@ int init_module()
 void cleanup_module()
 {
    int i;
+   struct log_block *block_ptr;
+   struct log_block *temp;
    for (i = 0; i < 30; i++) {
       unregister_kprobe(&(probes[i]));
+   }
+
+   for (block_ptr = head; block_ptr != NULL;) {
+      temp = block_ptr;
+      printk(KERN_INFO MODULE_NAME "Freeing block %d.\n", temp->id); block_ptr = block_ptr->next;
+      kfree(temp->lines);
+      kfree(temp);
    }
 
    printk(KERN_INFO MODULE_NAME "Cleaning up.\n");
